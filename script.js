2a88292cdbea7171a39a167c42ee4d4d46e3719f
Ruth Ellen <ruthellen1800@gmail.com>
2023-01-21 19:37:25 -0300

    Adicionado o Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+function enviar() {
+    let nome = document.getElementById("nomesobrenomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+}
